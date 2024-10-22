5dd0fd3a690e915a8f40f8a5311f7ef1802252cd
Antoine Honore <honore@kth.se>
2024-10-22 20:24:50 +0200

    minor

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ import matplotlib.pyplot as plt
 elu_feature_map = lambda x: torch.nn.functional.elu(x) + 1
 
 
-from src.dataset import compute_target, prep_data, plot_data, compute_target2
+from src.dataset import compute_target, prep_data, plot_data, compute_target2,compute_target3
 from src.model import CAMD
 from test import test
+from multiprocessing.dummy import Pool
+#from datetime import datetime
+
 
 if __name__ == "__main__":
     M = 3
-    Tmax = 1000
-    Dmax = 2
-    N = 1
-    d_out = 2
+    Tmax = 1000000
+    Dmax = 10
+    N = 2
+    d_out = 10
     #test()
     names = ["m{}".format(i+1) for i in range(M)]
 
@@ -36,11 +39,12 @@ if __name__ == "__main__":
 
     data = {k: torch.rand(N, t, d) for i,(k,t,d) in enumerate(zip(names, T, D))}
 
-    y = compute_target2(data, timelines, d_out)
-    device="cpu"
+    device = "cpu"
     if torch.cuda.is_available():
         torch.cuda.init()
         device = "cuda:0"
+
+    y = compute_target3(data, timelines, d_out).to(device)
 
     model =  CAMD(M, Dmax, Dmax, Dmax, n_layers=1, activation="relu", layernorm=False, skipconnections=True, skiptemperature=True).to(device)
     
@@ -51,14 +55,17 @@ if __name__ == "__main__":
     X = prep_data(data, timelines, device=device)
     
     num_epochs = 1000
-    every_e = num_epochs // 20
+    every_e = num_epochs // 100
+
     figsize = (15, 5)
-    if device=="cpu":
+    if device == "cpu":
         fig, ax = plt.subplots(figsize=figsize)
+    
     for epoch in range(num_epochs):
+        optimizer.zero_grad()
         yhat = model(X)
 
-        loss = torch.nn.functional.mse_loss(yhat, y.to(device))
+        loss = torch.nn.functional.mse_loss(yhat, y)
         loss.backward()
 
         optimizer.step()
@@ -66,8 +73,13 @@ if __name__ == "__main__":
         L.append(loss.item())
         if (epoch % every_e) == 0:
             print(epoch, L[-1])
-            if device=="cpu":
-                ax.cla()
-                _, images = plot_data(data, timelines,target=y[0],prediction=yhat[0].detach(),dim=0,figsize=figsize,masks=False,ax=ax)
-                plt.pause(0.5)
-    print("")
+            #start_epoch = datetime.now()
+
+        #    if device=="cpu":
+        #        ax.cla()
+        #        _, images = plot_data(data, timelines, target=y[0],prediction=yhat[0].detach(),dim=0,figsize=figsize,masks=False,ax=ax)
+        #        plt.pause(0.5)
+        
+        del loss
+        del yhat
+    print(epoch, L[-1], datetime.now(), start_epoch, "elapsed=", (datetime.now()-start_epoch).total_seconds())
