c357bdc4639984bfd3678aef47f391bf14496dc5
Antoine Honore <honore@kth.se>
2024-10-22 20:26:28 +0200

    multi variate and parralel

--- src/model.py
+++ src/model.py
@@ -1,14 +1,12 @@
 import torch
-from src.causal_product import causal_dot_product_ref as causal_dot_product
-from src.causal_product import causal_dot_product
+#from src.causal_product import causal_dot_product_ref as causal_dot_product
+from src.causal_product import causal_dot_product,causal_dot_product_ref
 
 
 from src.mlp import MLP
 from functools import partial
-import torch.multiprocessing as mp
-from multiprocessing.dummy import Pool
 
-#from multiprocessing import Process, Queue
+from multiprocessing.dummy import Pool
 
 
 class CAMD(torch.nn.Module):
@@ -24,8 +22,9 @@ class CAMD(torch.nn.Module):
         self.W_K = torch.nn.ModuleList([MLP(d_qk + 2, [d_qk+ 2]*n_layers,d_qk+ 2,activation,layernorm=layernorm, skipconnections=skipconnections, skiptemperature=skiptemperature) for _ in range(M)])
 
         self.W_out = torch.nn.Linear(M*d_v, d_out)
-        
-    def forward(self, calX):
+        self.pool = None
+
+    def forward(self, calX,pool=None):
         """
         calX is a dictionnary : {"m1":  shape (1,1,T_1,d_1), "m2":  shape (1,1,T_2,d_2), ...}
         """
@@ -33,12 +32,11 @@ class CAMD(torch.nn.Module):
         
         t1 = calX["m1"][0, 0, :, -1]
 
-        Zout = torch.zeros((1, self.M, t1.shape[0], self.d_qk+ 2), device=Q.device).share_memory_()
-        
         the_func = partial(forward_modality, t1=t1, Q=Q)
-
-        with Pool(self.M) as pool:
-            results = pool.map(the_func, zip(calX.values(), self.W_K))
+        if not (self.pool is None):
+            results = self.pool.map(the_func, zip(calX.values(), self.W_K))
+        else:
+            results = list(map(the_func, zip(calX.values(), self.W_K)))
         
         # Concatenate on the head dimension
         Zout = torch.cat(results, dim=1)
@@ -46,7 +44,7 @@ class CAMD(torch.nn.Module):
         # Flatten all the heads
         #Zout = Zout.transpose(1,2).flatten(start_dim=2,end_dim=3)
 
-        yhat = Zout.sum(1)[...,:2]
+        yhat = Zout.sum(1)[...,:self.d_out]
         
         return yhat
 
@@ -56,4 +54,10 @@ def forward_modality(args, t1=None, Q=None):
     K = W_k(X)
     V = X
     t2 = X[0,0,:,-1]
-    return causal_dot_product(Q, K, V, t1, t2)
+    causal_dot_product_func = causal_dot_product
+    
+    if t1.shape[0] == t2.shape[0]:
+        print("size(t1)=size(t2)=", t1.shape[0])
+        causal_dot_product_func = causal_dot_product_ref
+
+    return causal_dot_product_func(Q, K, V, t1, t2)
